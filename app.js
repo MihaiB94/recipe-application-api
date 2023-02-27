92acc70a8026ab178f4442d807d9a85f87969376
MihaiB94 <89844187+MihaiB94@users.noreply.github.com>
2023-02-27 00:16:05 +0000

    Update app.js

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.use(express.static(path.join(__dirname, '..', 'build')));
 app.use(express.static('public'));
 
 app.get('/*', function (req, res) {
-   res.sendFile(path.join(__dirname, 'build', '..', 'index.html'));
+   res.sendFile(path.join(__dirname, 'build',));
 });
 // Start listening to the server
 app.listen(port, () => {
