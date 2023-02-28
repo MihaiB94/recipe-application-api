0ad7db1141013d8c5605872616e01f8c68e3c29b
MihaiB94 <bormihverale@gmail.com>
2023-02-28 03:33:31 +0000

    trying to images

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const multer = require('multer');
 
 app.use(express.json());
 app.use(cors());
-app.use('/images', express.static(path.join(__dirname, '/images')));
+app.use('/images', express.static(path.join(__dirname, '/public/images')));
 
 // Connect to Mongo Database
 mongoose
