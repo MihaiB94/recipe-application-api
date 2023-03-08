8b7f013b94906758fe95405389cceac73d0656e1
MihaiB94 <89844187+MihaiB94@users.noreply.github.com>
2023-03-08 01:27:52 +0000

    Update recipes.js

--- routes/recipes.js
+++ routes/recipes.js
@@ -89,28 +89,33 @@ router.post('/', upload.single('file'), async (req, res) => {
 });
 
 //UPDATE A RECIPE
-router.put('/:id', async (req, res) => {
+router.put('/:id', upload.single('file'), async (req, res) => {
    try {
       const recipe = await Recipe.findById(req.params.id);
-      if (recipe.userId === req.body.userId) {
-         try {
-            const updatedRecipe = await Recipe.findByIdAndUpdate(
-               req.params.id,
-               {
-                  $set: req.body
-               },
-               {
-                  new: true
-               }
-            );
-            res.status(200).json(updatedRecipe);
-         } catch (err) {
-            res.status(500).json(err);
-         }
-      } else {
-         res.status(401).json('You cannot edit this recipe!');
+
+      // Make sure that the user is logged in and authorized to edit the recipe
+      if (recipe.userId !== req.user._id) {
+         return res
+            .status(401)
+            .json({ message: 'You are not authorized to edit this recipe.' });
       }
+
+      // Update the recipe data
+      recipe.username = req.body.username;
+      recipe.title = req.body.title;
+      recipe.categories = req.body.categories;
+      recipe.description = req.body.description;
+      recipe.ingredients = JSON.parse(req.body.ingredients);
+      recipe.preparation_steps = JSON.parse(req.body.preparation_steps);
+
+      if (req.file) {
+         recipe.image_url = req.file.location;
+      }
+
+      const updatedRecipe = await recipe.save();
+      res.status(200).json(updatedRecipe);
    } catch (err) {
+      console.log(err);
       res.status(500).json(err);
    }
 });
