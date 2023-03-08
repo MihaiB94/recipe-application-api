1d824d05de4967c0bdf93b04d54f4266b406de02
MihaiB94 <89844187+MihaiB94@users.noreply.github.com>
2023-03-08 03:13:17 +0000

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const uuid = require('uuid').v4;
 app.use(express.json());
 app.use(
    cors({
-      origin: 'http://localhost:3000',
+      origin: 'https://delicious-recipes.onrender.com',
       methods: ['GET', 'POST', 'PUT', 'DELETE'],
       credentials: true
    })
